f636bdffc5f1e0385af155999afbb60fef06cba9
申晗 <deepcoldy@gmail.com>
2018-05-11 14:45:11 +0800

    增加文件验证

---
+++ app/controller/auth.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class Auth extends Controller {
+  async file() {
+    this.ctx.body = 'gEaVKEDCW2TCQSaC';
+  }
+}
+
+module.exports = Auth;
